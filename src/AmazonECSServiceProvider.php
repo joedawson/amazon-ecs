791fed87b2ee4ed9156cf252e5a2eaef123a6548
Joe Dawson <hi@joedawson.me>
2017-02-28 10:05:09 +0000

    5.4 Compat

--- src/AmazonECSServiceProvider.php
+++ src/AmazonECSServiceProvider.php
@@ -7,18 +7,6 @@ use Illuminate\Support\ServiceProvider;
 class AmazonECSServiceProvider extends ServiceProvider
 {
 	/**
-	* Register bindings in the container.
-	*
-	* @return void
-	*/
-	public function register()
-	{
-		$this->app->bind('amazon-ecs', function($app) {
-			return new AmazonECS;
-		});
-	}
-
-	/**
 	* Perform post-registration booting of services.
 	*
 	* @return void
@@ -28,5 +16,15 @@ class AmazonECSServiceProvider extends ServiceProvider
 		$this->publishes([
 			__DIR__.'/../config/amazon.php' => config_path('amazon.php'),
 		], 'config');
+	}
+	
+	/**
+	* Register bindings in the container.
+	*
+	* @return void
+	*/
+	public function register()
+	{
+		$this->app->singleton(AmazonECS::class);
 	}
 }
